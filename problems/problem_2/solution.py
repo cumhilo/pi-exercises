1ac2f2d3687228667e34c0cd0f30fa4e188e44bd
Cumhilo <72714741+cumhilo@users.noreply.github.com>
2025-05-26 22:08:47 -0500

    perf: compile patterns & add a stop if it finds the necessary matches before finish the for loop

--- problems/problem_2/solution.py
+++ problems/problem_2/solution.py
@@ -1,21 +1,29 @@
 import re
 
+compiled_patterns = [re.compile(p) for p in [
+    r"[A-Z]",
+    r"[a-z]",
+    r"\d",
+    r"[^\w\s]"
+]]
+space_pattern = re.compile(r"\s")
+
+
 def check_password(password: str) -> bool:
     if len(password) < 8:
         return False
 
-    # truly this isn't necessary; looks good to me, making it easier to modify and add patterns, but
-    # in some way it's abuse of recursion more than expected
-    tests = sum(bool(re.search(i, password)) for i in [
-        r"[A-Z]",
-        r"[a-z]",
-        r"\d",
-        r"[^\w\s]"
-    ])
+    passed = 0
+    required = 3
+    for pattern in compiled_patterns:
+        if passed == required:
+            break
 
-    has_spaces = re.search(r"\s", password)
+        if re.search(pattern, password):
+            passed += 1
 
-    is_secure = tests >= 3
+    has_spaces = re.search(space_pattern, password)
+    is_secure = passed >= required
 
     if is_secure and not has_spaces:
         return True
